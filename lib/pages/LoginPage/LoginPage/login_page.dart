deede9fcd9f1453fac2813317aacebe3a30c9a02
Jaroslaw Gawlik <jargawl@gmail.com>
2022-07-04 21:13:47 +0200

    Implement Login Page Banner

--- lib/pages/LoginPage/LoginPage/login_page.dart
+++ lib/pages/LoginPage/LoginPage/login_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:life_wiev/pages/LoginPage/LoginPage/cubit/login_page_cubit.dart';
+import 'package:life_wiev/widgets/LoginPageBaner/login_page_banner.dart';
 
 class LoginPage extends StatefulWidget {
   LoginPage({
@@ -21,98 +23,136 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(isCreatingAccount == true
-                  ? 'Zarejestruj się'
-                  : 'Zaloguj się'),
-              const SizedBox(
-                height: 20,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            const LoginPageBanner(),
+            Text(
+              isCreatingAccount == true ? 'Register' : 'Sign in',
+              style: GoogleFonts.wellfleet(
+                color: Colors.lightBlue,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
               ),
-              TextField(
-                decoration: const InputDecoration(hintText: 'e-mail'),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: TextField(
+                decoration: const InputDecoration(
+                  hintText: 'e-mail',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                  ),
+                ),
                 controller: widget.emailController,
               ),
-              TextField(
-                decoration: const InputDecoration(hintText: 'password'),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: TextField(
+                decoration: const InputDecoration(
+                  hintText: 'password',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                  ),
+                ),
                 controller: widget.passwordController,
                 obscureText: true,
               ),
-              const SizedBox(
-                height: 20,
-              ),
-              Text(errorMesage),
-              BlocProvider(
-                create: (context) => LoginPageCubit(),
-                child: BlocBuilder<LoginPageCubit, LoginPageState>(
-                  builder: (context, state) {
-                    return ElevatedButton(
-                      onPressed: () async {
-                        if (isCreatingAccount == true) {
-                          try {
-                            await context
-                                .read<LoginPageCubit>()
-                                .createUserWithEmailAndPassword(
-                                    email: widget.emailController.text,
-                                    password: widget.passwordController.text);
-                          } catch (error) {
-                            setState(() {
-                              errorMesage = error.toString();
-                            });
-                          }
-
-                          // rejestracja
-                        } else {
-                          try {
-                            await context
-                                .read<LoginPageCubit>()
-                                .signInWithEmailAndPassword(
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Text(errorMesage),
+            BlocProvider(
+              create: (context) => LoginPageCubit(),
+              child: BlocBuilder<LoginPageCubit, LoginPageState>(
+                builder: (context, state) {
+                  return ElevatedButton(
+                    onPressed: () async {
+                      if (isCreatingAccount == true) {
+                        try {
+                          await context
+                              .read<LoginPageCubit>()
+                              .createUserWithEmailAndPassword(
                                   email: widget.emailController.text,
-                                  password: widget.passwordController.text,
-                                );
-                          } catch (error) {
-                            setState(() {
-                              errorMesage = error.toString();
-                            });
-                          }
-                          //logowanie
+                                  password: widget.passwordController.text);
+                        } catch (error) {
+                          setState(() {
+                            errorMesage = error.toString();
+                          });
                         }
-                      },
-                      child: Text(isCreatingAccount == true
-                          ? 'Zarejestruj się'
-                          : 'Zaloguj się '),
-                    );
-                  },
+
+                        // rejestracja
+                      } else {
+                        try {
+                          await context
+                              .read<LoginPageCubit>()
+                              .signInWithEmailAndPassword(
+                                email: widget.emailController.text,
+                                password: widget.passwordController.text,
+                              );
+                        } catch (error) {
+                          setState(() {
+                            errorMesage = error.toString();
+                          });
+                        }
+                        //logowanie
+                      }
+                    },
+                    child: Text(
+                      isCreatingAccount == true ? 'Register' : 'Sign in ',
+                      style: GoogleFonts.wellfleet(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            if (isCreatingAccount == false) ...[
+              TextButton(
+                onPressed: () {
+                  setState(() {
+                    isCreatingAccount = true;
+                  });
+                },
+                child: Text(
+                  'Create an account',
+                  style: GoogleFonts.wellfleet(
+                    color: Colors.lightBlue,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
               ),
-              const SizedBox(
-                height: 20,
-              ),
-              if (isCreatingAccount == false) ...[
-                TextButton(
-                  onPressed: () {
-                    setState(() {
-                      isCreatingAccount = true;
-                    });
-                  },
-                  child: const Text('Utwórz konto'),
-                ),
-              ],
-              if (isCreatingAccount == true) ...[
-                TextButton(
-                  onPressed: () {
-                    setState(() {
-                      isCreatingAccount = false;
-                    });
-                  },
-                  child: const Text('Masz juz konto'),
-                ),
-              ]
             ],
-          ),
+            if (isCreatingAccount == true) ...[
+              TextButton(
+                onPressed: () {
+                  setState(() {
+                    isCreatingAccount = false;
+                  });
+                },
+                child: Text(
+                  'Do you already have an account?',
+                  style: GoogleFonts.wellfleet(
+                    color: Colors.lightBlue,
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ]
+          ],
         ),
       ),
     );
---
+++ lib/widgets/LoginPageBaner/login_page_banner.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class LoginPageBanner extends StatelessWidget {
+  const LoginPageBanner({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height / 2.1,
+      child: Stack(
+        children: [
+          Image.network(
+            "https://images.photowall.com/products/60742/palm-trees-on-white-beach.jpg?h=699&q=85",
+            fit: BoxFit.cover,
+            height: 300,
+          ),
+          Align(
+            alignment: Alignment.center,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                const SizedBox(
+                  height: 50,
+                ),
+                Text(
+                  'Life',
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.wellfleet(
+                    color: Colors.white,
+                    fontSize: 50,
+                  ),
+                ),
+                Text(
+                  'View',
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.wellfleet(
+                    color: Colors.white,
+                    fontSize: 50,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
