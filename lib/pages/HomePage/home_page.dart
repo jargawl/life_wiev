1421b0780c22c63a874b8b6c30d74e4038fc1892
Jaroslaw Gawlik <jargawl@gmail.com>
2022-07-04 13:57:59 +0200

    Add Sing_out_button

--- lib/pages/HomePage/home_page.dart
+++ lib/pages/HomePage/home_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:life_wiev/widgets/EmotionCard/emotion_card.dart';
 import 'package:life_wiev/widgets/FadeAppBar/fade_app_bar.dart';
 import 'package:life_wiev/widgets/LifeViewCard/life_view_card.dart';
+import 'package:life_wiev/widgets/SingOutButton/sing_out_button.dart';
 import 'package:life_wiev/widgets/Topic/topic.dart';
 import 'package:life_wiev/widgets/LogoBanner/logo_banner.dart';
 
@@ -48,6 +49,7 @@ class _HomePageState extends State<HomePage> {
               ),
               Topic('A place to think about yourself.'),
               LifeViewList(),
+              SingOutButton(),
             ],
           ),
           FadeAppBar(scrollOffset: _scrollControllerOffset)
