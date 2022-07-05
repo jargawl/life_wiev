f59f8b23c453c5c4e982ff578e2981045f613f50
Jaroslaw Gawlik <jargawl@gmail.com>
2022-07-05 14:39:25 +0200

    FIx a problem with DreamsState

--- lib/widgets/LifeViewCard/life_view_card.dart
+++ lib/widgets/LifeViewCard/life_view_card.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:life_wiev/models/LifeView/life_view_model.dart';
-import 'package:life_wiev/pages/GratefulListPage/grateful_list_page.dart';
 
-class LifeViewList extends StatelessWidget {
+class LifeViewList extends StatefulWidget {
   const LifeViewList({Key? key}) : super(key: key);
 
+  @override
+  State<LifeViewList> createState() => _LifeViewListState();
+}
+
+class _LifeViewListState extends State<LifeViewList> {
   @override
   Widget build(BuildContext context) {
     return SliverToBoxAdapter(
@@ -31,43 +35,34 @@ class LifeViewCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () {
-        Navigator.of(context).push(
-          MaterialPageRoute(
-            builder: (_) =>  GratefulPage(),
+    return Container(
+      width: 300,
+      height: 100,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15.0),
+      ),
+      padding: const EdgeInsets.all(15),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          ClipRRect(
+              borderRadius: BorderRadius.circular(15.0),
+              child: Image.network(
+                lifeView.image,
+                fit: BoxFit.cover,
+              )),
+          const SizedBox(
+            height: 10,
           ),
-        );
-      },
-      child: Container(
-        width: 300,
-        height: 100,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(15.0),
-        ),
-        padding: const EdgeInsets.all(15),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            ClipRRect(
-                borderRadius: BorderRadius.circular(15.0),
-                child: Image.network(
-                  lifeView.image,
-                  fit: BoxFit.cover,
-                )),
-            const SizedBox(
-              height: 10,
+          Text(
+            lifeView.title,
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.w600,
+              color: Colors.black.withOpacity(.9),
             ),
-            Text(
-              lifeView.title,
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.w600,
-                color: Colors.black.withOpacity(.9),
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
