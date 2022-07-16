ff8c40516365aa63529255b082047cac33c8674f
Jaroslaw Gawlik <jargawl@gmail.com>
2022-07-16 20:10:32 +0200

    Add retrofit to dependencies

--- lib/models/QuotesModel/quotes_model.dart
+++ lib/models/QuotesModel/quotes_model.dart
@@ -15,36 +15,3 @@ class QuotesModel with _$QuotesModel {
 }
 
 
-
-
-// @JsonSerializable()
-// class QuotesModel {
-//   QuotesModel({
-//     required this.text,
-//     required this.author,
-//   });
-
-// @JsonKey(name: 'content')
-//   final String text;
-
-//   final String author;
-
-
-//   factory QuotesModel.fromJson(Map<String, dynamic> json) => _$QuotesModelFromJson(json);
-
-
-
-
-//    Map<String, dynamic> toJson() => _$QuotesModelToJson(this);
-// }
-
-
-
-
-
-//   // factory QuotesModel.fromJson(Map<String, dynamic> json) {
-//   //   return QuotesModel(
-//   //     text: json['content'],
-//   //     author: json['author'],
-//   //   );
-//   // }
